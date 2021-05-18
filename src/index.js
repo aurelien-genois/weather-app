14ffb72c885baf4975722d287f6c45bf713957c6
Smogo <aurelien.genois@gmail.com>
2021-05-18 16:21:39 +0200

    add plugin for create/load css on build

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import css from './style.css';
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Hello Formofo!';
+console.log(h1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,10 +8,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather app',
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   devtool: 'eval',
   module: {
     rules: [
-
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
 };
