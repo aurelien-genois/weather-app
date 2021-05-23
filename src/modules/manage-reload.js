b8b6ba20e085e4b6d7b4825d217bd471287e433b
Smogo <aurelien.genois@gmail.com>
2021-05-23 10:36:43 +0200

    Fixe manage-reload

--- src/modules/manage-reload.js
+++ src/modules/manage-reload.js
@@ -4,11 +4,11 @@ import { renderWeatherDisplay } from './render-weather-display.js';
 const manageReload = ((doc) => {
   /*** Manage Reload ***/
   const reloadBtn = doc.querySelector('#reload-btn');
-  const cityNameP = doc.querySelector('#city-name');
-  const cityNameText = cityNameP.textContent.slice(0, -2);
-  const countryIso = cityNameP.querySelector('.country-iso').textContent;
 
   reloadBtn.addEventListener('click', () => {
+    const cityNameP = doc.querySelector('#city-name');
+    const cityNameText = cityNameP.textContent.slice(0, -2);
+    const countryIso = cityNameP.querySelector('.country-iso').textContent;
     getWeather.getData(cityNameText, countryIso).then((data) => {
       renderWeatherDisplay.displayWeatherInfo(data);
     });
