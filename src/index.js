64fcacc5cb531f4640e713be59dcf49021063b4a
Aurélien Genois <aurelien.genois@gmail.com>
2025-06-20 21:06:02 +0200

    Deployment commit 2

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { getWeather } from './modules/get-weather.js';
-import { renderWeatherDisplay } from './modules/render-weather-display.js';
-
-// default value
-getWeather.getData('Taipei', 'TW').then((data) => {
-  renderWeatherDisplay.displayWeatherInfo(data);
-});
